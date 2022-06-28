3b35e951307f4c1f46ff40f540ad5240f9c414a9
daltonmitchell <dzack34@gmail.com>
2022-06-28 15:08:44 -0700

    adds Mood model and adds form for a new mood to new.ejs

---
+++ models/mood.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const moodSchema = new mongoose.Schema({
+    moodRating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
+    excitements: {
+        type: String
+    },
+    dreads: {
+        type: String
+    },
+    goals: {
+        type: String
+    },
+    journal: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Mood', moodSchema);
--- routes/moods.js
+++ routes/moods.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const moodsCtrl = require('../controllers/moods');
 
 router.get('/new', moodsCtrl.new);
+router.post('/', moodsCtrl.create);
 
 module.exports = router;
